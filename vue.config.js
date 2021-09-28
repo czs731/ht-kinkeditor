a2f384847ca378ea3eedaa7797f96221e1c6fb23
czs73 <czs731@163.com>
2021-09-28 11:24:44 +0800

    0.0.1

---
+++ vue.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+function resolve(dir) {
+  return path.resolve(__dirname, dir);
+}
+
+module.exports = {
+  devServer:{
+    port: 8080,
+    hot: true,
+    open: 'Google Chrome'
+  },
+  productionSourceMap: false,
+  pages: {
+    index: {
+      entry: "examples/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+    },
+  },
+  configureWebpack: {
+    output: {
+      libraryExport: "default",
+    },
+    resolve: {
+      extensions: [".js", ".vue", ".json"],
+      alias: {
+        "@": resolve("packages"),
+        assets: resolve("examples/assets"),
+        views: resolve("examples/views"),
+      },
+    },
+  },
+  css: {
+    extract: false,
+  },
+};
